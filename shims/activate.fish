07fc01692d7d8ee87aebd15cdbfa6d490cfa1049
Ed Hagerty <ed@hagerty.net>
2020-11-13 17:09:29 +0000

    edited the activate.fish file

--- shims/activate.fish
+++ shims/activate.fish
@@ -1,6 +1,6 @@
 [ -n "$PYENV_DEBUG" ] && set -x
-set -gx PYENV_ROOT "/Users/edhagerty/.pyenv"
-set -gx program "("/Users/edhagerty/.pyenv/libexec/pyenv" which "{BASH_SOURCE##*/}")"
+export PYENV_ROOT="/Users/edhagerty/.pyenv"
+program="$("/usr/local/Cellar/pyenv/1.2.21/libexec/pyenv" which "${BASH_SOURCE##*/}")"
 if [ -e "${program}" ]; then
   . "${program}" "$@"
 fi
