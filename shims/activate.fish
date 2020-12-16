f85fc47a855e4fa451b9df64c733039cc316daea
Ed Hagerty <ed@sweettreeconnect.com>
2020-12-16 16:29:27 +0000

    checkpoint

--- shims/activate.fish
+++ shims/activate.fish
@@ -1,6 +1,6 @@
 [ -n "$PYENV_DEBUG" ] && set -x
 export PYENV_ROOT="/Users/edhagerty/.pyenv"
-program="$("/usr/local/Cellar/pyenv/1.2.21/libexec/pyenv" which "${BASH_SOURCE##*/}")"
+program="$("/Users/edhagerty/.pyenv/libexec/pyenv" which "${BASH_SOURCE##*/}")"
 if [ -e "${program}" ]; then
   . "${program}" "$@"
 fi
