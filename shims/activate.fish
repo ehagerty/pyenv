495ed2118cf9f309bf9570c6af6240a697ff36e3
Ed Hagerty <ed@hagerty.net>
2021-07-12 17:16:19 +0100

    update

--- shims/activate.fish
+++
@@ -1,6 +0,0 @@
-[ -n "$PYENV_DEBUG" ] && set -x
-export PYENV_ROOT="/Users/edhagerty/.pyenv"
-program="$("/Users/edhagerty/.pyenv/libexec/pyenv" which "${BASH_SOURCE##*/}")"
-if [ -e "${program}" ]; then
-  . "${program}" "$@"
-fi
